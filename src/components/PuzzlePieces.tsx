c604169cf2a7f73c5a0d39d3f4b6cc839d50ad34
Rajeshr.developer <rajeshr.developer@gmail.com>
2022-02-20 02:12:03 +0530

    puzzle changes

--- src/components/PuzzlePieces.tsx
+++ src/components/PuzzlePieces.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react"
 import { Dimensions, Image, ImageSourcePropType, Pressable, View } from "react-native"
+import Draggable from "react-native-draggable";
 import data from '../../assets/bear_sprites/data';
 import { GameDimensions } from "../GameDimensions";
 
-export default function PuzzlePieces() {
+export default function PuzzlePieces(): JSX.Element {
 
     const [pos, setPos] = useState<{ x: number | undefined, y: number | undefined }>({ x: undefined, y: undefined });
 
     const [initialPos, setInitialPos] = useState({ initialX: 0, initialY: 0 });
+
+    const [isDrag, setDrag] = useState(false);
 
     return <View
         style={{
@@ -16,39 +19,27 @@ export default function PuzzlePieces() {
         }}>
         {
             data.sprites && data.sprites.map((n, indx) => {
-                return <Pressable
-                    onTouchStart={(__pos) => {
-                        setPos({ x: __pos.nativeEvent.pageX, y: __pos.nativeEvent.pageY })
-                        setInitialPos({ initialX: __pos.nativeEvent.locationX, initialY: __pos.nativeEvent.locationY })
+                return <View style={{
+                    position: "absolute",
+                    width: (n.width * GameDimensions.GAME_WIDTH_PER) / 100,
+                    height: (n.height * GameDimensions.GAME_HEIGHT_PER) / 100,
+                    left: n.left,
+                    top: n.top,
+                }}><Draggable
+                    shouldReverse={!0}
+                    touchableOpacityProps={{ activeOpacity: 1 }}
+                    onDrag={() => {
                     }}
-                    onTouchMove={(__pos) => {
-                        setPos({ x: __pos.nativeEvent.pageX, y: __pos.nativeEvent.pageY })
-                    }}
-                    onTouchEnd={() => {
-                        setPos({ x: undefined, y: undefined })
-                    }}
-                    onPress={() => {
-                        console.log('onPress')
-                    }}
-                    onPressIn={() => {
-                        console.log('onPressIn')
-                    }}
-                    style={{
-                        position: "absolute",
-                        width: (n.width * GameDimensions.GAME_WIDTH_PER) / 100,
-                        height: (n.height * GameDimensions.GAME_HEIGHT_PER) / 100,
-                        left: pos.x ? pos.x - initialPos.initialX : n.left,
-                        top: pos.y ? pos.y - initialPos.initialY : n.top,
-                    }}
-                ><Image
-                        key={indx}
-                        style={{
-                            width: (n.width * GameDimensions.GAME_WIDTH_PER) / 100,
-                            height: (n.height * GameDimensions.GAME_HEIGHT_PER) / 100
-                        }}
-                        source={n.src as ImageSourcePropType}
-                    />
-                </Pressable>
+                    onDragRelease={() => {
+                    }}><Image
+                            key={indx}
+                            style={{
+                                width: (n.width * GameDimensions.GAME_WIDTH_PER) / 100,
+                                height: (n.height * GameDimensions.GAME_HEIGHT_PER) / 100,
+                            }}
+                            source={n.src as ImageSourcePropType}
+                        />
+                    </Draggable></View>
             })
         }
     </View>
